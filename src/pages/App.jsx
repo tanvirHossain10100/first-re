2407a63f289a249a5199c507723fc8bbb81e7e00
tanvirHossain10100 <tanvirhossan528@gmail.com>
2023-11-25 11:11:10 +0600

    first commit

---
+++ src/pages/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+  return (
+    <>
+      <div className="countries">
+        <h1>The details and lis of the all country</h1>
+        {}
+      </div>
+    </>
+  );
+}
+
+export default App;
